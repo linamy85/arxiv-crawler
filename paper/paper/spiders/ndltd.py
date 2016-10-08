f924c9f30ab1c28b972a30c5f2ef04f2eba59821
Amy Lin <linamy85@gmail.com>
2016-10-08 18:29:25 +0800

    Parsing Bi version of title & abstract by order
    Can parse Chinese & English version of title & abstract
    by the given category chosen by user.
    
    Using selenium with scrapy... Due to dynamic element in the
    "next page" of the category list.
    
    May broke sometimes
    Guessing may be caused by not-yet-loaded problem?

--- paper/paper/spiders/ndltd.py
+++ paper/paper/spiders/ndltd.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
+
+# Need chromedriver & selenium to be installed!!
+
 import scrapy
 import pickle
 import sys
+from selenium import webdriver
+
 
 def user_select_category(list, layer):
     print("-------------------------------------------")
     print('Please select from the %dth-layer category.' % layer)
     for i in range(len(list)):
-        sys.stdout.write("%d %-10s\t" % (i, list[i]))
+        try:
+            sys.stdout.write(
+                "%d %-10s\t" % (i, list[i].find_element_by_xpath('./a/span').text)
+            )
+        except:
+            break
         if (i + 1) % 4 == 0:
             sys.stdout.write('\n')
     sys.stdout.write('\n')
@@ -20,73 +30,131 @@ def user_select_category(list, layer):
 class NdltdSpider(scrapy.Spider):
     name = "ndltd"
     allowed_domains = ["ndltd.ncl.edu.tw"]
-    start_urls = ( 
-        'http://ndltd.ncl.edu.tw/cgi-bin/gs32/gsweb.cgi/login?o=dwebmge', 
+    start_urls = (
+        'http://ndltd.ncl.edu.tw/cgi-bin/gs32/gsweb.cgi/login?o=dwebmge',
     )
     rooturl = 'http://ndltd.ncl.edu.tw'
 
+    def __init__(self):
+        self.chrome = webdriver.Chrome()
+        self.chrome.get(self.start_urls[0])
+
     def parse(self, response):
-        category_url = response.css(
-            'a[href*=browselevel]::attr(href)'
-        ).extract()[0]
+        self.chrome.get(self.start_urls[0])
+        category_url = self.chrome.find_element_by_xpath(
+            '//span[@class="schfunc"]/a[2]' # xpath is 1-based
+        ).get_attribute('href')
         print("found category_url: %s" % category_url)
-        req = scrapy.Request(self.rooturl + category_url,
-                             callback=self.select_category)
-        req.meta['layer'] = 0
-        return req
+        res = self.select_category(category_url, 0)
+        return res
 
-    def select_category(self, response):
-        layer = int(response.meta['layer'])
+    def select_category(self, abs_url, layer):
         print("++++++++++++ layer: %d" % layer)
-        table_ele = response \
-            .xpath('//table[@class="brwlv_table"]')[layer] \
-            .xpath('./tr/td[@class="brwlv_item_td"]')
+        self.chrome.get(abs_url)
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        print(abs_url)
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        table_ele = self.chrome.find_elements_by_xpath(
+            '(//table[@class="brwlv_table"])[' + str(layer + 1) + ']'
+            + '/tbody/tr/td[@class="brwlv_item_td"]'
+        )
+        print(table_ele)
+        if len(table_ele) == 0:
+            print("# no requested list.")
+            return None
 
-        href = table_ele.xpath('./a/@href').extract()
+        # href = table_ele.find_elements_by_xpath('./a').get_attribute('href')
         
-        category = table_ele.xpath('./a/span/text()').extract()
+        # category = table_ele.find_elements_by_xpath('./a/span').text
 
-        selected = user_select_category(category, layer)
-        if selected < 0 or selected >= len(category):
+        selected = user_select_category(table_ele, layer)
+        if selected < 0 or selected >= len(table_ele):
             print("Error input: %d" % selected)
-            return {} # Should close crawler immediately.
+            return None # Should close crawler immediately.
+
+        selected_href = table_ele[selected].find_element_by_xpath(
+            './a'
+        ).get_attribute('href')
 
         # reset layer
         if layer < 2:
-            req = scrapy.Request(self.rooturl + href[selected],
-                                 callback=self.select_category)
-            req.meta['layer'] = layer+1
-            return req
+            res = self.select_category(selected_href,
+                                       layer + 1)
+            return res
         else: # layer = 2
-            return scrapy.Request(self.rooturl + href[selected],
-                                  callback=self.parse_pages)
+            res = self.parse_pages(selected_href)
+            return res
 
-    def parse_pages(self, response):
+    def parse_pages(self, abs_url):
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        print(abs_url)
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        self.chrome.get(abs_url)
+        
+        url_list = []
+
         # parse papers in multiple pages.
-        article = response.xpath(
-            '//table[@class="tableoutsimplefmt2"]/tr/td/a[@class="slink"]'
-        )
-        for idx in range(len(article)):
-            link = article[idx].xpath('@href').extract_first()
-            title = article[idx].xpath('span').extract_first()
-            req = scrapy.Request(self.rooturl + link,
-                                 callback=self.parse_article)
-            req.meta['title'] = title
-            yield req
+        while True:
+            # Wait until 5 sec or page loaded.
+            # self.chrome.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+            article = self.chrome.find_elements_by_xpath(
+                '//table[@class="tableoutsimplefmt2"]'
+                '/tbody/tr/td/a[@class="slink"]'
+            )
+            print(article)
+            for a in article:
+                link = a.get_attribute('href')
+                # title = a.find_element_by_xpath('span').text
+                # print("$$$ title %s" % title)
+                # a.click()
+                # res = self.parse_article(link, title)
+                # yield res
+                url_list.append(link)
+            
+            # click next page button
+            try:
+                self.chrome.find_element_by_xpath(
+                    '//input[@name="gonext"]'
+                ).click()
+            except:
+                print("%%%%%%%% No page left. %%%%%%%%%%")
+                break
 
-    def parse_article(self, response):
+        # starts parsing pages
+        for url in url_list:
+            res = self.parse_article(url)
+            yield res
+
+    def parse_article(self, abs_url):
+        self.chrome.get(abs_url)
+        print(abs_url)
+        # get titles
+        ch_title = self.chrome.find_element_by_xpath(
+            '//table[@id="format0_disparea"]/tbody/tr[4]/td'
+        ).text
+        print(ch_title)
+
+        en_title = self.chrome.find_element_by_xpath(
+            '//table[@id="format0_disparea"]/tbody/tr[5]/td'
+        ).text
+        print(en_title)
+
         # parse single article.
-        chinese = response.xpath(
-            '//td[@class="stdncl2"]/div/text()'
-        )[0].extract()
-        english = response.xpath(
-            '//td[@class="stdncl2"]/div/text()'
-        )[1].extract()
+        chinese = self.chrome.find_element_by_xpath(
+            '(//td[@class="stdncl2"])[1]/div'
+        ).get_attribute('innerHTML')
+
+        english = self.chrome.find_element_by_xpath(
+            '(//td[@class="stdncl2"])[2]/div'
+        ).get_attribute('innerHTML')
+
+        # self.chrome.back()
 
         return {"post": {
-            "title": response.meta['title'],
-            "Chinese": chinese,
-            "English": english
+            "Chinese_title": ch_title,
+            "Chinese_abstract": chinese,
+            "English_title": en_title,
+            "English_abstract": english
         }}
 
 
